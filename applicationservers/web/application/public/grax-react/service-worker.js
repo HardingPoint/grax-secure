78358ef52722bc9dbf93b0510c4edc5b2cdff337
Brianna Kincart <briannakincart@gmail.com>
2019-12-31 08:41:04 -0500

    Update based on grax config Tue Dec 31 08:41:04 EST 2019

--- applicationservers/web/application/public/grax-react/service-worker.js
+++ applicationservers/web/application/public/grax-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c3e23db011c6a99bfffc49f145d1d4c.js"
+  "./precache-manifest.970feedf461ca7c9afebd3144124c506.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- config/default.js
+++ config/default.js
@@ -12,7 +12,8 @@ module.exports = {
     licenseToken: env.ENGAGEMENTGRAPH_LICENSETOKEN
   },
   backup: {
-    attachmentBatchSize: env.ATTACHMENT_BATCH_SIZE || 5000
+    attachmentBatchSize: env.ATTACHMENT_BATCH_SIZE || 5000,
+    processingSecondsPerRecord: env.ATTACHMENT_PROCESSING_SECONDS_PER_RECORD || 5
   },
   cipher: {
     salt: env.SALT || "528DB3A0C5F797426150EAE99927EFB960DB9579277A2CF0C95D11F29AB7BB7F"
