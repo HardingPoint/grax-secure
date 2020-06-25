7dc2b02aae73b1c63cfffaa7a97d52d287ff34b8
Travis CI User <travis@example.org>
2020-06-25 17:54:12 +0000

    Update based on grax config Thu Jun 25 17:54:12 UTC 2020

--- applicationservers/web/application/public/grax-react/service-worker.js
+++ applicationservers/web/application/public/grax-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grax-app/assets/precache-manifest.4a1a89a4672f111dc2488c013410c5a5.js"
+  "/grax-app/assets/precache-manifest.dfb1e5931f646f7dcf86a869e75a8134.js"
 );
 
 self.addEventListener('message', (event) => {
