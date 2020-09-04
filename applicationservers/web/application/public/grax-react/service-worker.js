180bd8f2701999b7ea1e81ab744a5b3415abb384
HardingPoint <Support@GRAX.io>
2020-09-04 12:40:44 +0000

    Update based on grax config Fri Sep  4 12:40:44 UTC 2020

--- applicationservers/web/application/public/grax-react/service-worker.js
+++ applicationservers/web/application/public/grax-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grax-app/assets/precache-manifest.720863679844d6678804480849e79424.js"
+  "/grax-app/assets/precache-manifest.5f0b7a3394c174680d997fcb15c85682.js"
 );
 
 self.addEventListener('message', (event) => {
