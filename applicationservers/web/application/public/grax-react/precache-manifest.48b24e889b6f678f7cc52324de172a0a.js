a371725b7b7983bb07ea74a7732251f43cf0abd3
HardingPoint <Support@GRAX.io>
2020-12-11 21:40:10 +0000

    Update based on grax config Fri Dec 11 21:40:10 UTC 2020

--- applicationservers/web/application/public/grax-react/precache-manifest.48b24e889b6f678f7cc52324de172a0a.js
+++ applicationservers/web/application/public/grax-react/precache-manifest.48b24e889b6f678f7cc52324de172a0a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2335b502e42105b2fff2a06415b5acb1",
+    "revision": "b0fb3dedf7c500afb036e1e485482bb2",
     "url": "/grax-app/assets/index.html"
   },
   {
