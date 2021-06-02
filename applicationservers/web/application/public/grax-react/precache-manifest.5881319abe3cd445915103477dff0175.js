d1abd0e7885f360b1023368c1e66d81d9f1fe863
HardingPoint <Support@GRAX.io>
2021-06-02 22:34:09 +0000

    Update based on grax config Wed Jun  2 22:34:09 UTC 2021

--- applicationservers/web/application/public/grax-react/precache-manifest.5881319abe3cd445915103477dff0175.js
+++ applicationservers/web/application/public/grax-react/precache-manifest.5881319abe3cd445915103477dff0175.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ef3357bfdc7962ec3c704ee892d9265",
+    "revision": "7cbc7777e027672f6edcea9edb877f1e",
     "url": "/grax-app/assets/index.html"
   },
   {
--- applicationservers/web/application/public/grax-react/service-worker.js
+++ applicationservers/web/application/public/grax-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grax-app/assets/precache-manifest.cc131430974a8a923ca1318419ebd656.js"
+  "/grax-app/assets/precache-manifest.5881319abe3cd445915103477dff0175.js"
 );
 
 self.addEventListener('message', (event) => {
